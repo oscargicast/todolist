7ef6e3b6f7c192556e6527cc1289c63d622f6736
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2015-07-11 21:00:08 -0500

    handle post request

--- src/runserver.py
+++ src/runserver.py
@@ -1,8 +1,13 @@
+import pymongo
+import tornado.escape
 import tornado.ioloop
 import tornado.web
-import pymongo
 
 from bson.json_util import dumps
+from tornado.options import define, parse_command_line
+
+define("debug", default=True, help="run in debug mode", type=bool)
+define("port", default=8000, help="run on the given port", type=int)
 
 
 class TaskHandler(tornado.web.RequestHandler):
@@ -15,6 +20,20 @@ class TaskHandler(tornado.web.RequestHandler):
         tasks = self.db.task.find()
         self.write(dumps({"Tasks": tasks}))
 
+    def post(self):
+        try:
+            data = tornado.escape.json_decode(self.request.body)
+        except:
+            data = {}
+        title = data.get("Title")
+        if title:
+            task = {
+                "Title": title,
+                "Done": False,
+            }
+            self.db.task.insert(task)
+        self.write("Hello, world")
+
 
 routers = [
     (r"/task/", TaskHandler),
@@ -25,6 +44,7 @@ application = tornado.web.Application(routers, debug=True)
 
 if __name__ == "__main__":
     print "Runserver..."
+    parse_command_line()
     application.connection = pymongo.MongoClient()
     application.listen(8000)
     tornado.ioloop.IOLoop.current().start()
